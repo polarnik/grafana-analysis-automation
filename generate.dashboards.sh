f7ba735be53382d8068752f7581d598624dcd1c4
Viacheslav Smirnov <viacheslav.smirnov@jetbrains.com>
2025-07-22 19:22:49 +0200

    Update

--- generate.dashboards.sh
+++ generate.dashboards.sh
@@ -4,19 +4,17 @@ BASE=$(pwd)
 WORKDIR=$BASE/target/jsonnet
 JSONNET=sjsonnet.jar
 
-jb install github.com/grafana/grafonnet/gen/grafonnet-v11.6.3@main
+jb install github.com/grafana/grafonnet/gen/grafonnet-v11.4.0@main
 
 
 if [ ! -f $WORKDIR ]; then
   mkdir -p $WORKDIR
 fi
 
-jb install github.com/grafana/grafonnet/gen/grafonnet-v11.6.3@main
-
 if [ ! -f $WORKDIR/$JSONNET ]; then
   echo "💤 Download sjsonnet"
   curl --output $WORKDIR/$JSONNET \
-  -L https://github.com/databricks/sjsonnet/releases/download/0.4.14/sjsonnet-0.4.14.jar
+  -L https://github.com/databricks/sjsonnet/releases/download/0.5.3/sjsonnet-0.5.3.jar
   echo "✅  Download sjsonnet complete"
 
   chmod +x $WORKDIR/$JSONNET
@@ -24,21 +22,32 @@ fi
 
 compile_a_dashboard () {
 
-array=( thanos_qf vm_promql)
+array_source_type=( thanos_qf vm_promql)
+diff_array=(current_prev current_several_prevs z_score)
+theme_array=(blue_white_red rainbow white_rainbow)
 
-for EXT_SOURCE_TYPE in "${array[@]}"
+for EXT_SOURCE_TYPE in "${array_source_type[@]}"
 do
-  echo "💤 $1.$EXT_SOURCE_TYPE"
-      "$WORKDIR/$JSONNET" \
-        --strict --strict-import-syntax \
-        --fatal-warnings --throw-error-for-invalid-sets \
-        --indent 4 \
-        --ext-str EXT_SOURCE_TYPE=$EXT_SOURCE_TYPE \
-        --no-duplicate-keys-in-comprehension \
-        -J "$WORKDIR/vendor"  \
-        --output-file "$2/$1.$EXT_SOURCE_TYPE.json" \
-        "$BASE/src/main/jsonnet/$1.jsonnet"
-  echo "✅  $1.$EXT_SOURCE_TYPE"
+  for EXT_DIFF_TYPE in "${diff_array[@]}"
+  do
+    for EXT_THEME in "${theme_array[@]}"
+    do
+      echo "💤 $EXT_SOURCE_TYPE/$EXT_DIFF_TYPE/$EXT_THEME/$1"
+          "$WORKDIR/$JSONNET" \
+            --strict   \
+            --fatal-warnings --throw-error-for-invalid-sets \
+            --indent 4 \
+            --ext-str EXT_SOURCE_TYPE=$EXT_SOURCE_TYPE \
+            --ext-str EXT_DIFF_TYPE=$EXT_DIFF_TYPE \
+            --ext-str EXT_THEME=$EXT_THEME \
+            -J vendor  \
+            --reverse-jpaths-priority \
+            --create-output-dirs \
+            --output-file "$2/$EXT_SOURCE_TYPE/$EXT_DIFF_TYPE/$EXT_THEME/$1.json" \
+            "$BASE/src/main/jsonnet/$1.jsonnet"
+      echo "✅  $EXT_SOURCE_TYPE/$EXT_DIFF_TYPE/$EXT_THEME/$1"
+    done
+  done
 done
 }
 
